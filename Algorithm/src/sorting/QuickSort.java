2e9e0b5fd923cf68c35f013a1f1c49621d35d258
yinqingjiang <yinqingjiang@10.21.130.210>
2018-06-24 15:08:13 -0700

    add quickSort

--- Algorithm/src/sorting/QuickSort.java
+++ Algorithm/src/sorting/QuickSort.java
@@ -1,5 +1,53 @@
 package sorting;
 
 public class QuickSort {
+	public static void main(String[] args) {
+		int[] nums=new int[]{7,3,9,11,7,4,2};
+		int low = 0;
+		int high = nums.length - 1;
+		quickSort(nums,low,high);
+		for(int i=0;i<nums.length;i++)
+		System.out.println(nums[i]);
+	}
+
+
+	public static void quickSort(int[] arr, int low, int high) {
+		if (arr == null || arr.length == 0)
+			return;
+ 
+		if (low >= high)
+			return;
+ 
+		// pick the pivot
+		int middle = low + (high - low) / 2;
+		int pivot = arr[middle];
+ 
+		// make left < pivot and right > pivot
+		int i = low, j = high;
+		while (i <= j) {
+			while (arr[i] < pivot) {
+				i++;
+			}
+ 
+			while (arr[j] > pivot) {
+				j--;
+			}
+ 
+			if (i <= j) {
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				i++;
+				j--;
+			}
+		}
+ 
+		// recursively sort two sub parts
+		if (low < j)
+			quickSort(arr, low, j);
+ 
+		if (high > i)
+			quickSort(arr, i, high);
+	}
 
 }
