e74a7b35f77e189a17a74cdce7b8ceb6fef16528
yinqingjiang <yinqingjiang@172.21.37.62>
2018-07-01 23:02:23 -0700

    add state vs strategy

--- Algorithm/src/ood/state/StopState.java
+++ Algorithm/src/ood/state/StopState.java
@@ -1,6 +1,12 @@
 package ood.state;
 
 public class StopState implements State {
+	/* State vs Strategy
+	 * States store a reference of context objects that contain them. Strategies do not.
+	 * Sates are allowed to replace themselves, while strategies are not.
+	 * Strategies are passed to context as parameters, while states are created by the context object itself.
+	 * Strategies only handle a single, specific task, while states provide underlying  implementation for everything the conext does
+	 */
 
 	@Override
 	public void doAction(Context context) {
